74d9663bdbe6ab46a4332ebba66d29140955e31d
Germain Boue <boue.germain@gmail.com>
2017-04-04 19:58:31 -0700

    Made some changes to connect NODE_ENV global variable to a test one so that we can dump collection

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const routes = require('./routes/routes');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 const app = express();
+
+mongoose.Promise = global.Promise;
+
+if (process.env.NODE_ENV !== 'test'){
+  mongoose.connect('mongodb://localhost/muber');
+}
 
 app.use(bodyParser.json());
 routes(app);
