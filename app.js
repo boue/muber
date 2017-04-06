dcaab588aa920a676fb40c4146e368effad57a85
Germain Boue <boue.germain@gmail.com>
2017-04-05 19:49:12 -0700

    Fixed syntax error

--- app.js
+++ app.js
@@ -9,8 +9,15 @@ mongoose.Promise = global.Promise;
 if (process.env.NODE_ENV !== 'test'){
   mongoose.connect('mongodb://localhost/muber');
 }
-
+//MIDDLEWARES
 app.use(bodyParser.json());
+//setup all the routes in application, routes are kind of like middleware
 routes(app);
+
+//.use is to build middleware. We are expecting the handler before to throw an error so we need 
+// err object, next is a function to go to next middleware in the chain
+app.use(() => {
+  console.log(err);
+});
 
 module.exports = app;
