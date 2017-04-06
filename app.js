8c8244851681db1a997fe1da41b6d7739abe0db7
Germain Boue <boue.germain@gmail.com>
2017-04-05 19:55:49 -0700

    Sending back an error to client via err object

--- app.js
+++ app.js
@@ -16,8 +16,8 @@ routes(app);
 
 //.use is to build middleware. We are expecting the handler before to throw an error so we need 
 // err object, next is a function to go to next middleware in the chain
-app.use(() => {
-  console.log(err);
+app.use((err, req, res, next) => {
+  res.send({ error: err.message });
 });
 
 module.exports = app;
